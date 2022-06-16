fb1f3f76974bddb4d5f1f206228a482f358ec558
knopgm <knop.gm@gmail.com>
2022-06-16 15:07:43 +0200

    Set React Router to visualize diferent pages of myFlix app

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import React from "react";
+import "./index.scss";
 import ReactDOM from "react-dom";
 import MainView from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
-
-import "./index.scss";
 
 //Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
@@ -17,7 +16,7 @@ class MyFlixApplication extends React.Component {
 }
 
 //Finds the root of your app
-const container = document.getElementsByClassName("app-container")[0];
+const container = document.getElementById("app-container");
 
 //Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
