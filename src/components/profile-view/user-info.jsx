6b100d2c6dd90228fce1d45e973f613d0e276eae
knopgm <knop.gm@gmail.com>
2022-06-22 22:26:23 +0200

    Set up the logic for deleting user register

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,15 +1,41 @@
 import React from "react";
+import axios from "axios";
 import { Button } from "react-bootstrap";
 
 export function UserInfo({ username, email, birthday }) {
-  // console.log(userData);
+  const onDeletingUserGoTo = () => {
+    localStorage.clear();
+    window.open("/");
+  };
+
+  const handlingDeletedUser = (username) => {
+    console.log("deleting");
+
+    const accessToken = localStorage.getItem("token");
+
+    if (!accessToken) {
+      return;
+    }
+
+    const url = `https://myflix-api-gkm.herokuapp.com/users/${username}`;
+
+    axios
+      .delete(url, { headers: { Authorization: `Bearer ${accessToken}` } })
+      .then(() => {
+        onDeletingUserGoTo();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <>
       <h4>Your Infos</h4>
       <p>Name: {username}</p>
       <p>e-mail: {email}</p>
       <p>Birthday: {birthday}</p>
-      <Button variant="secondary" onClick={() => console.log("deleting")}>
+      <Button variant="secondary" onClick={() => handlingDeletedUser(username)}>
         Delete Account
       </Button>
     </>
