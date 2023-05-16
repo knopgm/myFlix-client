7bc2bc46546074fe032593b030c3207392df02ff
knopgm <knop.gm@gmail.com>
2023-05-16 21:22:16 +0200

    Improve movie-card styles

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -56,47 +56,6 @@ export function MovieView(props) {
       </Col>
     </Row>
   );
-
-  return (
-    <Card className="movie-view">
-      <Card.Img
-        className="movie-view__img"
-        variant="top"
-        src={movie.imageUrl}
-      />
-
-      <Card.Body className="movie-view__body">
-        <Card.Title className="movie-view__title">{movie.title}</Card.Title>
-        <Link className="movie-view__link" to={`/genre/${movie.genre.name}`}>
-          {movie.genre.name}
-        </Link>
-        <Card.Text className="movie-view__description">
-          Description: {movie.description}
-        </Card.Text>
-        <Link
-          className="movie-view__link"
-          to={`/directors/${movie.director.name}`}
-        >
-          Director: {movie.director.name}
-        </Link>
-
-        <ListGroup className="movie-view__body-buttons">
-          <Button
-            className="movie-view__body-button"
-            onClick={() => addToFavMovies()}
-          >
-            Favorite
-          </Button>
-          <Button
-            className="movie-view__body-button"
-            onClick={() => onBackClick()}
-          >
-            Back
-          </Button>
-        </ListGroup>
-      </Card.Body>
-    </Card>
-  );
 }
 
 MovieView.propTypes = {
